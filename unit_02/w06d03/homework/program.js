b90de5864d783f9703dc78e6799383ecaa331559
Ray Meibaum <rmeibaum@me.com>
2017-03-15 17:33:55 -0400

    homework complete 3/15/17

---
+++ unit_02/w06d03/homework/program.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+
+app.get('/home', (req, res) => {
+  res.end('Hello World!');
+});
+
+app.get('/search', (req, res) => {
+  res.send(req.query);
+})
+
+app.post('/form', (req, res) => {
+  res.end(req.body.str.split('').reverse().join(''));
+});
+
+app.put('/message/:id', (req, res) => {
+  res.end(crypto.createHash('sha1')
+                .update(new Date().toDateString() + req.params.id)
+                .digest('hex'));
+});
+
+app.listen(process.argv[2]);
