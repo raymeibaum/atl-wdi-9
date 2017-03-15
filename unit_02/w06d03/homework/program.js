1d3d05072a4c3252ebd1378dba478c9791415aff
Ray Meibaum <rmeibaum@me.com>
2017-03-15 17:50:14 -0400

    minor formatting changes

--- unit_02/w06d03/homework/program.js
+++ unit_02/w06d03/homework/program.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 var crypto = require('crypto');
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
   extended: true
