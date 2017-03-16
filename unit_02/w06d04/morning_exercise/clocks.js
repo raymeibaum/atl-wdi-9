4f3c50a2707d286815fde0d386852e90754ed404
Ray Meibaum <rmeibaum@me.com>
2017-03-16 10:13:27 -0400

    failed to pass all tests

--- unit_02/w06d04/morning_exercise/clocks.js
+++ unit_02/w06d04/morning_exercise/clocks.js
@@ -1,5 +1,15 @@
 var calculate = function(hourhand, minutehand) {
+  if (hourhand > 12 || hourhand < 0 || minutehand > 60 || minutehand < 0) {
+    return 'out of range'
+  }
+  var totalMinutes = hourhand * 60 + minutehand;
+  if (totalMinutes > 720) {
+    totalMinutes -= 720;
+  }
+  var percentOfMinutesInClock = totalMinutes / 720 //720 minutes in 12 hours
+  var hourHandDegress = percentOfMinutesInClock * 360;
 
+  return [hourHandDegress, 360 - hourHandDegress];
 };
 
 // DO NOT EDIT BELOW THIS LINE //
